1cad8359a39c50fe66f91617e64a92c458a40b10
savanikrishna95 <savanitina@outlook.com>
2024-01-24 18:18:50 +0530

    added code

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -72,7 +72,7 @@ const Products = () => {
   const ShowProducts = () => {
     return (
       <>
-        <div className="buttons text-center py-5">
+        <div className="buttons text-center py-5 womenadclothing">
           <button className="btn btn-outline-dark btn-sm m-2" onClick={() => setFilter(data)}>All</button>
           <button className="btn btn-outline-dark btn-sm m-2" onClick={() => filterProduct("men's clothing")}>Men's Clothing</button>
           <button className="btn btn-outline-dark btn-sm m-2" onClick={() => filterProduct("women's clothing")}>
